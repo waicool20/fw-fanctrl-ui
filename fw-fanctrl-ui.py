41a87519229a12f4c626de72cc68c3266e36a590
waicool20 <waicool20@gmail.com>
2025-04-18 20:41:24 -0700

    Add function to check if fw-fanctrl is alive and update menu accordingly

--- fw-fanctrl-ui.py
+++ fw-fanctrl-ui.py
@@ -1,55 +1,61 @@
+import re
+from threading import Thread
+import time
 from pystray import MenuItem, Menu
+from pystray._base import Icon
 import pystray
 from PIL import Image
 import subprocess
 
-program_name="Framework Fan Control UI"
+program_name = "Framework Fan Control UI"
 currentStrategy = ""
 
-def is_current_strategy(item):
+
+def is_current_strategy(item: MenuItem):
   global currentStrategy
   return item.text == currentStrategy
 
-def set_strategy(icon, item):
+
+def set_strategy(icon: Icon, item: MenuItem):
   global currentStrategy
   currentStrategy = subprocess.check_output(["fw-fanctrl", "use", item.text]).decode("utf-8").strip()
   icon.notify(f"{currentStrategy}")
 
+
 def generate_strategy_menu():
   for i in subprocess.check_output(["fw-fanctrl", "print", "list"]).decode("utf-8").splitlines():
-    if i.startswith("Strategy list:"): 
+    if not i.startswith("-"):
       continue
-    cleanedName = i.strip()
-    cleanedName = i.lstrip(" - ")
-    yield MenuItem(cleanedName, set_strategy, checked=is_current_strategy, radio=True)
-    
-def reload(icon, item):
+    yield MenuItem(i[2:], set_strategy, checked=is_current_strategy, radio=True)
+
+
+def reload(icon: Icon, item: MenuItem):
   result = subprocess.check_output(["fw-fanctrl", "reload"]).decode("utf-8").strip()
   icon.notify(f"Reload: {result}")
-    
-def pause(icon, item):
+
+
+def pause(icon: Icon, item: MenuItem):
   result = subprocess.check_output(["fw-fanctrl", "pause"]).decode("utf-8").strip()
   icon.notify(f"Pause: {result}")
-  
-def resume(icon, item):
+
+
+def resume(icon: Icon, item: MenuItem):
   result = subprocess.check_output(["fw-fanctrl", "resume"]).decode("utf-8").strip()
   icon.notify(f"Resume: {result}")
-  
+
+
 def quit():
+  icon.stop()
   exit(0)
+
 
 def generate_main_menu():
   global currentStrategy
-  try:
-    commandOutput = subprocess.check_output(["fw-fanctrl", "print", "current"]).decode("utf-8").strip()
-    commandOutput = commandOutput.splitlines()[0]
-    commandOutput = commandOutput.replace('Strategy in use:', '');
-    commandOutput = commandOutput.strip().strip("'")
-    currentStrategy = commandOutput
-  except:
-    yield MenuItem("fw-fanctrl not installed", action=None, enabled=False)
+  if currentStrategy == "":
+    yield MenuItem("fw-fanctrl daemon not installed or running", action=None, enabled=False)
+    yield MenuItem("Quit", quit)
     return
-  
+
   yield MenuItem(program_name, action=None, enabled=False)
   yield MenuItem(f"Current Strategy: {currentStrategy}", action=None, enabled=False)
   yield MenuItem("Set Strategy", Menu(generate_strategy_menu))
@@ -58,11 +64,30 @@ def generate_main_menu():
   yield MenuItem("Resume", resume)
   yield MenuItem("Quit", quit)
 
-image=Image.open("favicon.ico")
-icon=pystray.Icon("name", image, program_name, Menu(generate_main_menu))
 
-def setup(icon):
-    icon.visible = True
+def check_alive():
+  global icon, currentStrategy
+  while True:
+    try:
+      commandOutput = subprocess.check_output(["fw-fanctrl", "print", "current"],
+                                              stderr=subprocess.STDOUT).decode("utf-8").strip()
+      _currentStrategy = re.findall(r"Strategy in use: '(.*?)'", commandOutput)[0]
+      if currentStrategy != _currentStrategy:
+        currentStrategy = _currentStrategy
+        icon.update_menu()
+    except:
+      currentStrategy = ""
+      icon.update_menu()
+    time.sleep(2)
+
+
+image = Image.open("favicon.ico")
+icon = pystray.Icon("name", image, program_name, Menu(generate_main_menu))
+Thread(target=check_alive, name="CheckAliveThread").start()
+
+
+def setup(icon: Icon):
+  icon.visible = True
+
 
 icon.run(setup)
-
